a369bd18a50ed78a035982da7b3e05e644bce4cd
J.P. Cummins <jp@syncd.co>
2022-07-26 15:59:51 -0700

    checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users do
+    member do
+      get :auth
+      post :is_authorized
+    end
+  end
+
   root 'public#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
