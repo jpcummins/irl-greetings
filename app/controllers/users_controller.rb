f36ba4d4aa1344beefe57439eae6b4ff325abdd4
J.P. Cummins <jp@syncd.co>
2022-07-28 13:51:22 -0700

    sanitize input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,7 +68,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.where("id = ?", params[:id].to_i).first
     end
 
     # Only allow a list of trusted parameters through.
